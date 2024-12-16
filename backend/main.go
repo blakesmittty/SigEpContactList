e2977801238efeb80170e3845b6c28dd03d54c26
Blake <blakeas12@gmail.com>
2024-12-16 18:49:04 -0500

    sheets set up

--- backend/main.go
+++ backend/main.go
@@ -9,7 +9,15 @@ import (
 	"github.com/joho/godotenv"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
+
+type Contact struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Email       string
+}
 
 // func determineSemester() string {
 // 	var semester string
@@ -51,7 +59,50 @@ func initGoogleDriveClient() {
 
 	}
 
+	mostRecentCensusId := r.Files[0].Id
+	fmt.Printf("most recent: %v\n", mostRecentCensusId)
+
+	initGoogleSheetsClient(mostRecentCensusId, ctx, serviceAccKey)
+
 }
+
+func initGoogleSheetsClient(censusId string, ctx context.Context, serviceAccKey string) {
+	var contacts []Contact
+
+	sheetsService, err := sheets.NewService(ctx, option.WithCredentialsFile(serviceAccKey))
+	if err != nil {
+		log.Fatal("couldn't create google sheets client")
+	}
+	resp, err := sheetsService.Spreadsheets.Values.Get(censusId, "Form Responses 1!B2:G250").Do()
+	if err != nil {
+		log.Fatalf("couldnt get census values: %v", err)
+	}
+
+	if len(resp.Values) == 0 {
+		fmt.Println("no data found")
+	}
+
+	for _, row := range resp.Values {
+		if len(row) == 0 {
+			fmt.Println("found empty row")
+			break
+		}
+
+		contacts = append(contacts, Contact{
+			FirstName:   row[0].(string),
+			LastName:    row[1].(string),
+			PhoneNumber: row[4].(string),
+			Email:       row[5].(string),
+		})
+	}
+
+	fmt.Printf("contacts: %v\n", contacts)
+
+}
+
+// func getContacts(w http.ResponseWriter, r *http.Request) {
+
+// }
 
 func main() {
 	err := godotenv.Load()
